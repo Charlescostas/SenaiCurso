47ae3e151b588be60c208a522aef78aa4d16e3b5
charlescostas@gmail.com <Mak808312*>
2024-03-20 20:24:55 -0300

    Exercicio 2

--- trello/M1S07/exe_1.js
+++ trello/M1S07/exe_1.js
@@ -1,10 +1,33 @@
-const numeros = [1,2,3,4,5,6,7,8,9,10];
-const frutas = ["maça","morango","manga"];
+var listaNumeros = [1,2,3,4,5,6,7,8,9,10];
+var listaFrutas = ["maça","morango","manga"];
 
-numeros.forEach(numero => {
+listaNumeros.forEach(numero => {
     console.log(`Numero: ${numero}`);
 });
 
-frutas.forEach(fruta => {
+listaFrutas.forEach(fruta => {
+    console.log(`Fruta: ${fruta}`);
+});
+
+//exercicio 2
+
+listaNumeros.push(15);
+listaNumeros.forEach(numero => {
+    console.log(`Numero: ${numero}`);
+});
+
+
+listaNumeros.pop();
+listaNumeros.forEach(numero => {
+    console.log(`Numero: ${numero}`);
+});
+
+listaFrutas.unshift("Banana");
+listaFrutas.forEach(fruta => {
+    console.log(`Fruta: ${fruta}`);
+});
+
+listaFrutas.shift();
+listaFrutas.forEach(fruta => {
     console.log(`Fruta: ${fruta}`);
 });
