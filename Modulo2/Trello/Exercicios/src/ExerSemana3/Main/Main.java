e983d028c5627223f64b5720aa0e1c9a04a120c1
charlescostas@gmail.com <Mak808312*>
2024-06-20 19:37:39 -0300

    [M2S03] Mini Projeto - Parte 01 - Criando a Classe Básica

---
+++ Modulo2/Trello/Exercicios/src/ExerSemana3/Main/Main.java
@@ -0,0 +1,6 @@
+package ExerSemana3.Main;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ Modulo2/Trello/Exercicios/src/ExerSemana3/Main/domain/Livro.java
@@ -0,0 +1,69 @@
+package ExerSemana3.Main.domain;
+
+public class Livro {
+    private String ISBN;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private Integer anoPublicacao;
+    private String genero;
+
+    public Livro(){
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "ISBN='" + ISBN + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", editora='" + editora + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+}
