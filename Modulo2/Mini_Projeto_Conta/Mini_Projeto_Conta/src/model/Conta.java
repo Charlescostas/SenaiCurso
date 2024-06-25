96d7a63034f06b194b06099b2f7dbba8de2efa78
charlescostas@gmail.com <Mak808312*>
2024-06-25 20:10:25 -0300

    [M02S04] Mini Projeto Parte 01: Criando a classe base

---
+++ Modulo2/Mini_Projeto_Conta/Mini_Projeto_Conta/src/model/Conta.java
@@ -0,0 +1,27 @@
+package model;
+
+public abstract class Conta {
+    private final String titular;
+    private double saldo;
+
+    protected Conta(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor){
+        this.saldo = getSaldo() + valor;
+    };
+
+    public abstract void sacar(double valor);
+
+    public abstract void exibirDetalhes();
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
